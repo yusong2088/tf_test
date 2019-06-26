745441c516ead5b05ffc4fc035dd5c5251cf2e4c
yusong <yuson2088@gmail.com>
2019-06-26 16:25:41 +0800

    MNIST

--- tf_8.2.py
+++ tf_8.2.py
@@ -1,3 +1,4 @@
+'''
 import tensorflow as tf
 import numpy as np
 import  matplotlib.pyplot as plt
@@ -7,3 +8,29 @@ with tf.Session() as sess:
     ax.plot(tf.random_normal([100]).eval(),tf.random_normal([100]).eval(),'o')
     ax.set_title('sample random plot for TensorFlow')
     plt.savefig("result.png")
+'''
+'''
+import tensorflow as tf
+import numpy as np
+#import scipy as sc
+#import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from matplotlib import  pyplot
+
+
+
+
+centers=[(-2,-2),(-2,1.5),(1.5,-2),(2,1.5)]
+fig, ax =pyplot.subplots()
+data,features=make_blobs(n_samples=200,centers=centers,n_features=2,
+                         cluster_std=0.8,shuffle=False,
+                         random_state=42)
+ax.scatter(np.asarray(data).transpose()[0],np.asarray(data).transpose()[1],marker='o',s=250)
+pyplot.show()
+'''
+from sklearn import datasets
+iris=datasets.load_iris()
+digits=datasets.load_digits()
+print (iris)
+print (digits)
+print(digits.data),digits.data
